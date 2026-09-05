bfd5ae9d52ef0206d6e3ec8f7482c8f870731883
Wojtek <wojka66@gmail.com>
2026-09-05 22:25:30 +0200

    Initial Spring Boot setup.

---
+++ src/test/java/property_manager/PropertyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package property_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
